2aa09b4c8980a86816ad823640a5ac390cf4cb50
Dev <devendranaga884@gmail.com>
2024-12-24 22:31:35 +0530

    fix bindtodevice setting
    Signed-off-by: Dev <devendranaga884@gmail.com>

--- idsm/src/lib/net/net_socket.rs
+++ idsm/src/lib/net/net_socket.rs
@@ -2,6 +2,8 @@
 
 use std::mem::MaybeUninit;
 
+use crate::lib::c_lib::c_strcpy;
+
 pub fn net_socket_create(family : i32, sock_type : i32, protocol : i32) -> i32 {
     unsafe {
         return libc::socket(family, sock_type, protocol);
@@ -14,16 +16,21 @@ pub fn net_socket_close(sock : i32) {
     }
 }
 
-pub fn net_socket_bind_to_device(sock : i32) -> i32{
-    let ret;
-    let bind_to_device : u32 = 0;
+pub fn net_socket_bind_to_device(sock : i32, ifname : &String) -> i32 {
+    let mut ret;
 
     unsafe {
-        ret = libc::setsockopt(sock,
-                               libc::SOL_SOCKET,
-                               libc::SO_BINDTODEVICE,
-                               &bind_to_device as *const u32 as *const libc::c_void,
-                               4);
+        let mut req : libc::ifreq = MaybeUninit::zeroed().assume_init();
+
+        c_strcpy::c_strcpy(&mut req.ifr_name, ifname.as_str());
+        ret = libc::ioctl(sock, libc::SIOCGIFINDEX, &req);
+        if ret == 0 {
+            ret = libc::setsockopt(sock,
+                                   libc::SOL_SOCKET,
+                                   libc::SO_BINDTODEVICE,
+                                   &req as *const libc::ifreq as *const libc::c_void,
+                                   4);
+        }
     }
 
     return ret;
